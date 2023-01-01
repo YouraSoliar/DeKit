60f05bfba656802d9f08247c56307d973a7a35d4
kospleyer <kospleyer@gmail.com>
2023-01-01 23:24:23 +0200

    Add: prgress bar

--- app/src/main/java/com/example/dekit/ui/base/BaseActivity.java
+++ app/src/main/java/com/example/dekit/ui/base/BaseActivity.java
@@ -1,13 +1,18 @@
 package com.example.dekit.ui.base;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.dekit.R;
+import com.example.dekit.ui.progress_bar.ProgressBarDialog;
 
 public class BaseActivity extends AppCompatActivity {
+
+    private ProgressBarDialog progressBarDialog = null;
 
     @Override
     public void onBackPressed() {
@@ -15,6 +20,30 @@ public class BaseActivity extends AppCompatActivity {
             super.onBackPressed();
         } else {
             finish();
+        }
+    }
+
+    private FrameLayout getProgressContainer() {
+        return findViewById(R.id.progress_container);
+    }
+
+    public void showProgressBarDialog() {
+        if (getProgressContainer() != null) {
+            getProgressContainer().setVisibility(View.VISIBLE);
+        } else {
+            if (progressBarDialog == null) {
+                progressBarDialog = new ProgressBarDialog();
+                progressBarDialog.show(getSupportFragmentManager(), null);
+            }
+        }
+    }
+
+    public void hideProgressBarDialog() {
+        if (getProgressContainer() != null) {
+            getProgressContainer().setVisibility(View.GONE);
+        } else {
+            progressBarDialog.dismiss();
+            progressBarDialog = null;
         }
     }
 
